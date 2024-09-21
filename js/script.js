96c945e66f79cee14b035e5a7239b05144a259b7
Kangue Loic <kangueloic9@gmail.com>
2024-09-21 23:48:28 +0100

    Upload du reste du projet

--- js/script.js
+++ js/script.js
@@ -1,34 +1,17 @@
-document.addEventListener("DOMContentLoaded", function () {
-    const cards = document.querySelectorAll('.card-sponsor');
-    const container = document.querySelector('.cards.carrusel');
+/* Open */
+function openNav() {
+    document.getElementById("myNav").style.width = "100%";
+  }
+  
+  /* Close */
+  function closeNav() {
+    document.getElementById("myNav").style.width = "0%";
+  }
 
-    const scaleCards = () => {
-        // Get the center position of the container
-        const containerRect = container.getBoundingClientRect();
-        const containerCenter = containerRect.left + containerRect.width / 2;
+  var copy = document.querySelector(".logos-slide").cloneNode(true);
+  document.querySelector(".logos").appendChild(copy);
 
-        cards.forEach(card => {
-            const cardRect = card.getBoundingClientRect();
-            const cardCenter = cardRect.left + cardRect.width / 2;
-            const distanceFromCenter = Math.abs(containerCenter - cardCenter);
-
-            if (distanceFromCenter < 100) {
-                // Center card
-                card.style.transform = 'scale(1)';
-            } else if (distanceFromCenter < 300) {
-                // Adjacent cards
-                card.style.transform = 'scale(0.90)';
-            } else {
-                // Furthermost cards
-                card.style.transform = 'scale(0.80)';
-            }
-        });
-    };
-
-    // Add event listeners to detect scrolling and resizing
-    container.addEventListener('scroll', scaleCards);
-    window.addEventListener('resize', scaleCards);
-
-    // Initial scaling
-    scaleCards();
-});
+  window.addEventListener("scroll", function(){
+    var header = this.document.querySelector("header");
+    header.classList.toggle("sticky", window.scrollY > 0)
+  });
